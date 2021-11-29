d99163c8167e8ae9609d723a33b73eb7d607ad4a
Ann Vasilyeva <skazkadreamanna@gmail.com>
2021-11-29 19:35:11 +0300

    add icon

--- index.js
+++ index.js
@@ -3,22 +3,24 @@ require('dotenv').config()
 const text = require('./const');
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply(`Hi, ${ctx.message.from.first_name ? 
-  ctx.message.from.first_name : 'stranger'}!`))
-bot.help((ctx) => ctx.reply(text.commands))
-
-bot.command('course', async (ctx)=> {
+bot.start((ctx) => {
+  ctx.reply(`Hi, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'stranger'}!`)
   try {
-    await ctx.replyWithHTML('<b>Show info about:</b>', Markup.inlineKeyboard(
+    ctx.replyWithHTML('<b>Show info about:</b>', Markup.inlineKeyboard(
       [
-        [Markup.button.callback('HTML', 'btn_1'), Markup.button.callback('CSS', 'btn_2')],
-        [Markup.button.callback('JS', 'btn_3'), Markup.button.callback('REACT', 'btn_4')]
+        [Markup.button.callback('\xF0\x9F\x92\x8E Маркетинговые материалы', 'btn_1'), Markup.button.callback('\xF0\x9F\x93\x84	 Шаблоны документов', 'btn_2')],
+        [Markup.button.callback('Наши ресурсы', 'btn_3'), Markup.button.callback('Дайджест', 'btn_4')]
       ]
     ))
   } catch(e) {
     console.error(e)
   }  
 })
+bot.help((ctx) => ctx.reply(text.commands))
+
+// bot.command('course', async (ctx)=> {
+  
+// })
 
 function addActionBot(name, src, text) {
   bot.action(name, async (ctx) => {
